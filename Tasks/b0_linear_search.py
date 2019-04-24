215d3a820bbc6592e48f0a0a84f86571ebf9bc4b
Евдокимова Татьяна Владимировна <TATEVD@avalon.ru>
2019-04-24 21:44:44 +0300

    binary_search

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -12,10 +12,9 @@ def min_search(arr) -> int:
 	:return: index of first occurrence of minimal element in array
 	"""
 
-
 	num = 0
 	for i in range(len(arr)):
 		if arr[i] < arr[num]:
 			num = i
-	return arr[num]
+	return num
 
--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -5,6 +5,19 @@ def binary_search(elem, arr):
 	:param elem: element to be found
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
-	"""
-
-	return None
+		"""
+	left_b = -1
+	right_b = len(arr)-1
+	while right_b > left_b+1:
+		middle = (left_b + right_b)//2
+		if arr[middle] > elem:
+			right_b = middle
+		else:
+			left_b = middle
+			
+	if elem == arr[right_b]:
+		return right_b
+	elif elem == arr[left_b]:
+		return left_b
+	else:
+		return None
